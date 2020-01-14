13c80577a0058dd4d6d37148cc120c31159763df
yuhannakapali <yuhannakapali@gmail.com>
2020-01-14 12:59:32 +0545

    answer are appering and removed no console rule

--- src/components/questionbox.vue
+++ src/components/questionbox.vue
@@ -9,10 +9,20 @@
 
     <hr class="my-4">
 
-     <!-- <p v-for="(answer, index) in answers" :key="index">
-     {{answers}} 
+    
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index" 
+          @click="selectanswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+        >
+          {{ answer }}
 
-    </p> -->
+        </b-list-group-item>
+      
+      </b-list-group>
+    
 
     <b-button variant="primary" href="#">submit</b-button>
     <b-button @click="$emit('next')" variant="success" href="">next</b-button>
@@ -22,17 +32,64 @@
 
 <script>
 export default {
+  
+ 
   props:{
     currentQuestion: Object,
     next : Function
+
+  },
+  data(){
+    return {
+      selectedIndex: null
+    }
   },
   computed: {
     answers(){
-      let answers = [...this.currentQuestion.incorrect_answer]
-      return answers.push(this.currentquestion.correct_answer)
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  watch:{
+    currentQuestion(){
+      this.selectedIndex= null
+      
+    }
+  },
+  methods: {
+    selectanswer(index){
+      this.selectedIndex= index
+      console.log(index)
     }
   }
     
 
 }
 </script>
+
+<style  scoped>
+.list-group{
+  margin-bottom : 15px;
+}
+.list-group-item:hover{
+  background: #eee;
+}
+
+
+.btn{
+  margin: 0 5px;
+}
+
+.selected {
+  background: lightblue;
+}
+.correct{
+  background: lightgreen;
+
+}
+
+.incorrect{
+  background: red;
+}
+</style>
